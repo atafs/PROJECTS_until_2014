4f82a8657d735512abc5c984c27386fa797c9c0f
atafs <atafs@iscte-iul.pt>
2015-02-28 05:19:36 +0000

    c

---
+++ LinguagemJAVA_SE_2POO_projectSIMConsumoElectrico_Pack V1.0/src/pt/iscte/poo/main/MainIntecalar.java
@@ -0,0 +1,57 @@
+package pt.iscte.poo.main;
+
+import pt.iscte.poo.instalacao.Instalacao;
+import pt.iscte.poo.instalacao.Relogio;
+import pt.iscte.poo.instalacao.aparelhos.Lampada;
+
+public class MainIntecalar {
+	
+	private static final int END_T = 1000;
+
+	public static void main(String[] args) {
+		Instalacao casa = new Instalacao();
+		casa.novaLinha("cozinha", 4); 
+		casa.novaLinha("quartos", 3);
+		casa.novaLinha("sala", 3);
+		
+		Lampada lamp1 = new Lampada("lamp1", 20);
+		casa.ligaAparelho("cozinha", lamp1);
+		
+		System.out.println(casa);
+		
+		int t = 0;
+		for (; t != END_T / 4; t++) {
+			Relogio.getInstanciaUnica().tique();			
+		}
+
+		lamp1.liga();
+		System.out.println(casa);
+		
+		for (; t != END_T / 2; t++) {
+			Relogio.getInstanciaUnica().tique();
+		}
+
+		Lampada lamp2 = new Lampada("lamp2", 30);
+		casa.ligaAparelho("cozinha", lamp2);
+		lamp2.liga();
+		
+		Lampada lamp3 = new Lampada("lamp1", 40);
+		casa.ligaAparelho("quartos", lamp3);
+		lamp3.liga();
+		
+		System.out.println(casa);
+		
+		for (; t != 3 * END_T / 4; t++) {
+			Relogio.getInstanciaUnica().tique();
+		}
+		
+		lamp2.desliga();
+		System.out.println(casa);
+		
+		for (; t != END_T; t++) {
+			Relogio.getInstanciaUnica().tique();
+		}
+		System.out.println(casa);
+	}
+
+}
