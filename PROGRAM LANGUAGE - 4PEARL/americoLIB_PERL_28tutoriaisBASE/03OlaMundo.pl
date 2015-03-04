cd01870d4625c0e98eb035653d033a8107808d6c
atafs <atafs@iscte-iul.pt>
2015-03-04 00:10:55 +0000

    c

--- PROGRAM LANGUAGE - 4PEARL/americoLIB_PERL_28tutoriaisBASE/03OlaMundo.pl
+++
@@ -1,9 +0,0 @@
-#e assim que se coloca comentarios#
-
-
-#variáveis#
-$variable = "Ola Mundo \n";
-print $variable;
-
-$variable = "Estou a mudar o valor \n";
-print $variable;
--- PROGRAM LANGUAGE - 4PEARL/americoLIB_PERL_28tutoriaisBASE/04Constants.pl
+++
@@ -1,9 +0,0 @@
-#Constants#
-
-use constant pi => 3.14;
-
-#imprimir#
-print "\n";
-print 3 * pi;
-print "\n";
-
